bdc1ee01843a0d2fc6afe5e8d232ccc1c5259088
theabsparrow <absbashar04@gmail.com>
2025-12-22 22:55:22 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "@/component/HomePage";
+
+export default function Home() {
+  return (
+    <section>
+      <HomePage />
+    </section>
+  );
+}
---
+++ component/HomePage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-[linear-gradient(40deg,hsl(0_0%_0%)_2%,hsl(285_65%_8%)_34%,hsl(278_72%_13%)_46%,hsl(278_72%_13%)_57%,hsl(285_65%_8%)_69%,hsl(0_0%_0%)_99%)] text-white p-6">
+      this is home page
+    </div>
+  );
+};
+
+export default HomePage;
